fd69334a1ab6321045897c85e807bfeeafb55b0e
JEagles1 <jeagles1@sheffield.ac.uk>
2022-05-17 16:40:04 +0100

    lmao

--- src/task5.py
+++ src/task5.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+import math
+from tokenize import Triple
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
@@ -6,7 +9,6 @@ from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 from math import degrees
 from enum import Enum
-from random import randint
 
 from sensor_msgs.msg import LaserScan
 
@@ -24,7 +26,11 @@ class State(Enum):
     LD = 3
     RD = 4
     SW = 5
-    END = 6
+    ROOM = 6
+    TURN = 7
+    TURNRIGHT = 8
+    TURNLEFT = 9
+    END = 10
 
 class task3:
 
@@ -40,12 +46,24 @@ class task3:
         self.posx = 0.0
         self.posy = 0.0
         self.yaw  = 0.0
+        self.startup = True
+        self.crop_x = 0
+        self.crop_y = 0
+        self.state = State.SW
 
+        self.x = 0.0
+        self.y = 0.0
+        self.theta_z = 0.0
+        self.x0 = 0.0
+        self.y0 = 0.0
+        self.theta_z0 = 0.0
+        
         self.subscriber = rospy.Subscriber('/odom', Odometry, self.odom_convert)
+       
+
         self.publisher  = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.vel_cmd    = Twist()
-
-
+        
         self.rate            = rospy.Rate(10)
         self.scan_subscriber = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
 
@@ -71,47 +89,31 @@ class task3:
             print(e)
         
         height, width, _ = cv_img.shape
-        crop_width = width - 800
-        crop_height = 100
-        crop_x = int((width/2) - (crop_width/2))
-        crop_y = int((height/2) - (crop_height/2))
+        self.crop_width = width
+        self.crop_height = 100
+        self.crop_x = int((width/2) - (self.crop_width/2))
+        self.crop_y = int((height/2) - (self.crop_height/2))
 
-        crop_img = cv_img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
-        hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+        crop_img = cv_img[self.crop_y:self.crop_y+self.crop_height, self.crop_x:self.crop_x+self.crop_width]
+        self.hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
 
         # create a single mask to accommodate all four dectection colours:
         for i in range(4):
             if i == 0:
-                mask = cv2.inRange(hsv_img, self.lower[i], self.upper[i])
+                mask = cv2.inRange(self.hsv_img, self.lower[i], self.upper[i])
             else:
-                mask = mask + cv2.inRange(hsv_img, self.lower[i], self.upper[i])
+                mask = mask + cv2.inRange(self.hsv_img, self.lower[i], self.upper[i])
 
         m = cv2.moments(mask)
         
         self.m00 = m["m00"]
         self.cy = m["m10"] / (m["m00"] + 1e-5)
 
-        hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-        lower_threshold =  (115, 224, 100)
-        upper_threshold = (130, 255, 255)
-        img_mask = cv2.inRange(hsv_img, lower_threshold, upper_threshold)
-        filtered_img_blue = cv2.bitwise_and(crop_img, crop_img, mask = img_mask)
-
-        lower_threshold =  (25, 150, 100)
-        upper_threshold = (70, 255, 255)
-        img_mask = cv2.inRange(hsv_img, lower_threshold, upper_threshold)
-        filtered_img_green = cv2.bitwise_and(crop_img, crop_img, mask = img_mask)
-
-        lower_threshold =  (0, 185, 100)
-        upper_threshold = (10, 255, 255)
-        img_mask = cv2.inRange(hsv_img, lower_threshold, upper_threshold)
-        filtered_img_red = cv2.bitwise_and(crop_img, crop_img, mask = img_mask)
-
         lower_threshold =  (75, 150, 100)
         upper_threshold = (100, 255, 255)
-        img_mask = cv2.inRange(hsv_img, lower_threshold, upper_threshold)
+        img_mask = cv2.inRange(self.hsv_img, lower_threshold, upper_threshold)
         filtered_img_turquoise = cv2.bitwise_and(crop_img, crop_img, mask = img_mask)
-       
+        self.hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
         if self.m00 > self.m00_min  :
             cv2.circle(crop_img, (int(self.cy), 200), 10, (0, 0, 255), 2)   
 
@@ -127,9 +129,12 @@ class task3:
     
    # Function for moving speed setting
     def move_speed_setting(self, linear = 0.0, angular = 0.0):
-        self.vel_cmd.linear.x  = linear * (3 / 4)
-        self.vel_cmd.angular.z = angular * (1 / 3)
+        self.vel_cmd.linear.x  = linear * (3 / 5)
+        self.vel_cmd.angular.z = angular * (1 / 2)
 
+    def turn(self, turn = 1):
+        self.vel_cmd.linear.x = 0
+        self.vel_cmd.angular.z = 0.5 * turn
 
     #Function for data publishing
     def publish(self):
@@ -176,9 +181,9 @@ class task3:
 
         #elif (forward <= front_threshold):
             #if (left >= right or front_left >= front_right):
-                #return State.LT
-            #else:
-                #return State.RT
+              #  return State.TURNLEFT
+           # else:
+             #   return State.TURNRIGHT
 
         #elif (right >= side_threshold and right >= left and front_right >= alpha * 1.5):
             #return State.RT
@@ -202,6 +207,26 @@ class task3:
         self.yaw  = self.round(degrees(yaw), 4)
         self.posx = self.round(position.x, 4)
         self.posy = self.round(position.y, 4)
+        # obtain the orientation and position co-ords:
+        or_x = odom_data.pose.pose.orientation.x
+        or_y = odom_data.pose.pose.orientation.y
+        or_z = odom_data.pose.pose.orientation.z
+        or_w = odom_data.pose.pose.orientation.w
+        pos_x = odom_data.pose.pose.position.x
+        pos_y = odom_data.pose.pose.position.y
+
+        # convert orientation co-ords to roll, pitch & yaw (theta_x, theta_y, theta_z):
+        (roll, pitch, yaw) = euler_from_quaternion([or_x, or_y, or_z, or_w], 'sxyz')
+        
+        self.x = pos_x
+        self.y = pos_y
+        self.theta_z = yaw 
+
+        if self.startup:
+            self.startup = False
+            self.x0 = self.x
+            self.y0 = self.y
+            self.theta_z0 = self.theta_z
 
     def stop(self):
         self.move_speed_setting()
@@ -222,40 +247,49 @@ class task3:
         while not (self.ranges):
             self.rate.sleep()
         while True:
-            state = self.state_cal(self.ranges)
-            back_left = np.amin(np.array(self.ranges[115:145]))
-            back_right = np.amin(np.array(self.ranges[215:245]))
-            front_right = np.amin(np.array(self.ranges[295:335]))
-            front_left = np.amin(np.array(self.ranges[25:65]))
-            corner_backs = np.amin(np.append(back_left, back_right))
-            corner_fronts = np.amin(np.append(front_left, front_right))
-            if (state == State.SW):
-                front_right = self.ranges[-55]
-                e  = 0.4 - front_right
-                kp = 3
-                self.move_speed_setting(0.25, kp * e)
-                #self.move_speed_setting(0.25, 0)
-
-            elif (state == State.RD):
-                self.move_speed_setting(0.2, -0.9)
-
-            elif (state == State.RT):
-                self.move_speed_setting(0, -0.9)
-
-            elif (state == State.LD):
-                self.move_speed_setting(0.2, 0.9)
-
-            elif (state == State.LT):
-                self.move_speed_setting(0, 0.9)
-
-            elif (state == State.END):
-                self.move_speed_setting(-0.05, 1.5)
-
+            print(self.state)
+            if(self.state == State.TURN):
+                print(self.theta_z)
+                if(abs(self.theta_z0 - self.theta_z) >= math.pi):
+                    self.state = State.SW
+                else:
+                    self.turn()
             else:
-                self.move_speed_setting(0, 0)
+                self.state = self.state_cal(self.ranges)
+                
+                back_left = np.amin(np.array(self.ranges[115:145]))
+                back_right = np.amin(np.array(self.ranges[215:245]))
+                front_right = np.amin(np.array(self.ranges[295:335]))
+                front_left = np.amin(np.array(self.ranges[25:65]))
+                corner_backs = np.amin(np.append(back_left, back_right))
+                corner_fronts = np.amin(np.append(front_left, front_right))
+                if (self.state == State.SW):
+                    front_right = self.ranges[-55]
+                    e  = 0.4 - front_right
+                    kp = 3
+                    self.move_speed_setting(0.25, kp * e)
+                    #self.move_speed_setting(0.25, 0)
 
-            if (corner_backs <= 0.2 and corner_fronts >= 0.2):
-                self.vel_cmd.linear.x = 0.1
+                elif (self.state == State.RD):
+                    self.move_speed_setting(0.2, -0.9)
+
+                elif (self.state == State.RT):
+                    self.move_speed_setting(0, -0.9)
+
+                elif (self.state == State.LD):
+                    self.move_speed_setting(0.2, 0.9)
+
+                elif (self.state == State.LT):
+                    self.move_speed_setting(0, 0.9)
+
+                elif (self.state == State.END):
+                    self.move_speed_setting(-0.05, 1.5)
+
+                else:
+                    self.move_speed_setting(0, 0)
+
+                if (corner_backs <= 0.2 and corner_fronts >= 0.2):
+                    self.vel_cmd.linear.x = 0.1
             self.publish()
             self.rate.sleep()
 
