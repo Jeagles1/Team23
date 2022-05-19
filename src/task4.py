ebd10e6f975f33ba0979938e36ec2b341fe32454
JEagles1 <jeagles1@sheffield.ac.uk>
2022-05-19 19:15:09 +0100

    task4

--- src/task4.py
+++ src/task4.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 # Import the core Python modules for ROS and to implement ROS Actions:
+from random import randint
 from turtle import distance
 import numpy as np
 import rospy
@@ -9,7 +10,7 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
-from math import degrees
+from math import degrees, pi
 from enum import Enum
 
 from sensor_msgs.msg import LaserScan
@@ -32,6 +33,8 @@ class State(Enum):
     BEACON = 6
     FINDCOLOUR = 7
     END = 8
+    ZE = 9
+    RM = 10
 
 
 class colour_search(object):
@@ -54,10 +57,26 @@ class colour_search(object):
         self.vel_cmd = Twist()
 
         self.pub.publish()
+        
+        self.start_yaw = 0
+
         self.posx = 0.0
         self.posy = 0.0
-        self.yaw = 0
-        self.start_yaw = 0
+        self.yaw  = 0.0
+        self.startup = True
+        self.crop_x = 0
+        self.crop_y = 0
+        self.state = State.SW
+
+        self.x = 0.0
+        self.y = 0.0
+        self.theta_z = 0.0
+        self.x0 = 0.0
+        self.y0 = 0.0
+        self.theta_z0 = 0.0
+
+        self.targetturn = 0
+        self.targetdirection = 0
 
         self.lasttime = rospy.get_rostime()
         self.lasttimeran = 0
@@ -88,17 +107,6 @@ class colour_search(object):
 
         self.state = State.FINDCOLOUR
 
-    #Function to round number
-    def round(self, value, precision):
-        value = int(value * (10**precision))
-
-        return float(value) / (10**precision)
-
-   # Function for moving speed setting
-    def move_speed_setting(self, linear = 0.0, angular = 0.0):
-        self.vel_cmd.linear.x  = linear 
-        self.vel_cmd.angular.z = angular
-
     def distance_to_Beacon(self, r):
         forward  = r[0]
         return forward
@@ -111,37 +119,32 @@ class colour_search(object):
         right  = r[-55]
         return right
 
-    def state_cal(self, r):
-        forward  = r[0]
-        left  = r[90]
-        right  = r[-90]
-        backward  = r[180]
-        front_right = r[-55]
-        front_left = r[55]
+    def round(self, value, precision):
+        value = int(value * (10**precision))
 
-        alpha           = 0.4
-        side_threshold  = 0.7
-        front_threshold = 0.6
-        back_threshold  = 0.8
+        return float(value) / (10**precision)
 
-        r_prime    = np.array(r)
-        close_count = float(np.count_nonzero(r_prime<=back_threshold)) / 360.0 * 100.0
+    
+   # Function for moving speed setting
+    def move_speed_setting(self, linear = 0.0, angular = 0.0):
+        self.vel_cmd.linear.x  = linear * (3 / 4)
+        self.vel_cmd.angular.z = angular * (1 / 3)
 
-       
-        if (close_count > 75 and forward <= front_threshold and front_right <= alpha * 1.5):
-            return State.END
-        elif (forward <= front_threshold and left <= side_threshold and right <= side_threshold and front_left <= alpha * 1.5 and front_right <= alpha * 1.5):
-            return State.END
-        elif (forward <= front_threshold and left <= side_threshold and right <= side_threshold and front_left >= alpha * 1.5 and front_right <= alpha * 1.5):
-            return State.LD
-        elif (right <= side_threshold and front_right >= alpha * 1.5):
-            return State.RD
-        elif (right >= side_threshold and front_right >= alpha * 1.5):
-            return State.RT
-        elif (forward <= front_threshold and left >= side_threshold and right <= side_threshold and front_left >= alpha * 2.5 and front_right <= alpha * 1.5):
-            return State.LT
-        else:
-            return State.SW
+
+    #Function for data publishing
+    def publish(self):
+        self.publisher.publish(self.vel_cmd)
+
+    #Function of convert data
+    def odom_convert(self, odom_data):
+        orientation = odom_data.pose.pose.orientation
+        position    = odom_data.pose.pose.position
+        (_, _, yaw) = euler_from_quaternion([orientation.x,
+            orientation.y, orientation.z, orientation.w],'sxyz') 
+
+        self.yaw  = self.round(degrees(yaw), 4)
+        self.posx = self.round(position.x, 4)
+        self.posy = self.round(position.y, 4)
 
     def stop(self):
         self.move_speed_setting()
@@ -156,6 +159,28 @@ class colour_search(object):
         self.pub.publish(self.vel_cmd)
         cv2.destroyAllWindows()
         self.ctrl_c = True
+
+    #When entering the random movement state
+    def enteringCompleteRandom(self):
+        self.theta_z0 = self.yaw
+        self.targetturn = randint(90, 180)
+        self.targetdirection = randint(0,1)
+        if self.targetdirection == 0:
+            self.targetdirection = -1
+
+    def enteringInformedRandom(self):
+        self.theta_z0 = self.yaw
+        # highest_a = np.argmax(self.ranges)
+        rangestuff = np.where(np.array(self.ranges) < 1, 10, np.array(self.ranges))
+        lowest_a = np.argmin(rangestuff)
+        if lowest_a > 180:
+            self.targetdirection = -2.5
+        else:
+            self.targetdirection = 2.5
+        if lowest_a == 180:
+            self.targetturn = 180
+        else:
+            self.targetturn = lowest_a % 180
 
     def callback_function(self, odom_data):
         angular_x = odom_data.pose.pose.orientation.x
@@ -214,7 +239,7 @@ class colour_search(object):
         for i in range(6):
             if(self.lower[i][0]<=hsv_img[25][25][0] and self.upper[i][0]>=hsv_img[25][25][0] and self.lower[i][1]<=hsv_img[25][25][1] and self.upper[i][1]>=hsv_img[25][25][1] and self.target_colour == "" and (self.lasttime.secs - self.lasttimeran >= 6)):
                 self.target_colour = (self.colours[i])
-                self.state = State.END
+                self.state = State.RM
                 print("Target colour is:" + self.target_colour)
                 # Thresholds for ["Blue", "Red", "Green", "Turquoise", "Purple","Yellow"]
                 self.target_colour = "Red"
@@ -284,26 +309,63 @@ class colour_search(object):
                 else:
                     self.vel_cmd.linear.x = 0.2
                 self.pub.publish(self.vel_cmd)
+                
+            
             else:
-                self.state = self.state_cal(self.ranges)
+                forward = np.sum(np.append(np.array(self.ranges[0:15]), np.array(self.ranges[345:360])))
+            front = np.amin(np.append(np.array(self.ranges[0:30]), np.array(self.ranges[330:360])))
+            left = np.sum(self.ranges[30:90])
+            right = np.sum(self.ranges[270:330])
+            l = self.ranges[30:90]
+            l = l[l != 0]
+            minleft = np.amin(l)
+            #minleft = np.amin(self.ranges[30:90][self.ranges[30:90] != 0])
+            #minright = np.amin(self.ranges[270:330][self.ranges[270:330 != 0]])
+            r = self.ranges[270:330]
+            r = r[r != 0]
+            minright = np.amin(r)
+            minsides = np.amin(np.append(l,r))
+            print(minleft - minright)
+            front_left = np.amin(np.array(self.ranges[35:45]))
+            front_right = np.amin(np.array(self.ranges[315:325]))
+            mid_left = np.amin(np.array(self.ranges[45:55]))
+            mid_right = np.amin(np.array(self.ranges[305:315]))
+            if (self.state == State.ZE):
+                # Zone Exploring
+                if (front_left <= 0.26 or front_right <= 0.26 or front <= 0.23):
+                    # About to hit a wall
+                    if ((front_left - front_right <= 0.1 and front_left - front_right >= -0.1) or (front_left >= 0.7 and front_right >= 0.7)):
+                        # If it thinks its stuck when moving away, try turning, possibly change this to random
+                        #self.move_speed_setting(-0.05, (left - right + 0.2) * 0.5 * (1 / forward))
+                        print("Stuck")
 
-                if (self.state == State.SW):
-                    front_right = self.ranges[-55]
-                    e  = 0.4 - front_right
-                    kp = 3
-                    self.move_speed_setting(0.25, kp * e)
-
-                elif (self.state == State.RD or self.state == State.RT):
-                    self.move_speed_setting(0.25, -1.2)
-
-                elif (self.state == State.LD or self.state == State.LT):
-                    self.move_speed_setting(0.25, 1.5)
-
-                elif (self.state == State.END):
-                    self.move_speed_setting(0, 1.5)
-
+                        self.enteringCompleteRandom()
+                        #self.enteringInformedRandom()
+                        self.state = State.RM
+                    #elif ((front_left <= 0.22 or front_right <= 0.22) and front > 0.3):
+                        #self.move_speed_setting(forward * 0.005, 0)
+                    else:
+                        # If not, just move back
+                        self.move_speed_setting(-0.2, 0)
                 else:
+                    # If not about to hit a wall
+                    if (minleft - minright <= 0.03 and minleft - minright >= -0.03):
+                        # If it thinks its in a 'corridoor', then move just forward
+                        print("yeah")
+                        #self.move_speed_setting((forward * 0.005), 1 * (1 / forward))
+                        self.move_speed_setting((forward * 0.005), 0)
+                    else:
+                        # If it is not in a corridoor, move based on zone in front of and around it
+                        self.move_speed_setting((forward * 0.005), (((left * mid_left) - (right * mid_right)) * 1.5 * (1 / (forward + 1)) * minsides))
+            elif (self.state == State.RM):
+                # Random Movement
+                if abs(self.theta_z0 - self.yaw) >= ((self.targetturn / 180) * pi):
+                    # Stop and go back to zone exploration
                     self.move_speed_setting(0, 0)
+                    self.state = State.ZE
+                else:
+                    # Keep Turning
+                    self.move_speed_setting(-0.05, self.targetdirection)
 
             self.publish()
             self.rate.sleep()
