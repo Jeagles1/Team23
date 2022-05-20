7e1485dee919fb143342afcd129792ac6d93c0ef
Harry Bolton <hjbolton1@sheffield.ac.uk>
2022-05-20 18:21:30 +0100

    Task 4 mostly done

--- src/task4.py
+++ src/task4.py
@@ -21,6 +21,7 @@ from cv_bridge import CvBridge, CvBridgeError
 # Import all the necessary ROS message types:
 from sensor_msgs.msg import Image
 
+c_width = 1500
 
 class State(Enum):
     
@@ -214,6 +215,14 @@ class colour_search(object):
         self.posx = self.round(position.x, 4)
         self.posy = self.round(position.y, 4)
     
+    def mask_colour(self, hsv_img, col):
+        mask = cv2.inRange(hsv_img, self.lower[col], self.upper[col])
+        m = cv2.moments(mask)
+        
+        self.m00 = m["m00"]
+        
+        self.cy = m["m10"] / (m["m00"] + 1e-5)
+    
     def camera_callback(self, img_data):
         try:
             cv_img = self.cvbridge_interface.imgmsg_to_cv2(img_data, desired_encoding="bgr8")
@@ -221,7 +230,7 @@ class colour_search(object):
             print(e)
         
         height, width, _ = cv_img.shape
-        crop_width = 1000
+        crop_width = c_width
         crop_height = 50
         crop_x = int((width/2) - (crop_width/2))
         crop_y = int((height/2) - (crop_height/2))
@@ -229,17 +238,23 @@ class colour_search(object):
 
         crop_img = cv_img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
         hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+        
 
+        if (self.target_colour == "Blue"):
+            self.mask_colour(hsv_img, 0)
         if (self.target_colour == "Red"):
-            mask = cv2.inRange(hsv_img, self.lower[1], self.upper[1])
-            m = cv2.moments(mask)
-        
-            self.m00 = m["m00"]
-        
-            self.cy = m["m10"] / (m["m00"] + 1e-5)
+            self.mask_colour(hsv_img, 1)
+        if (self.target_colour == "Green"):
+            self.mask_colour(hsv_img, 2)
+        if (self.target_colour == "Turquoise"):
+            self.mask_colour(hsv_img, 3)
+        if (self.target_colour == "Purple"):
+            self.mask_colour(hsv_img, 4)
+        if (self.target_colour == "Yellow"):
+            self.mask_colour(hsv_img, 5)
 
         """
-        # create a single mask to accommodate all four dectection colours:
+        # create a single mask to accommodate all four detection colours:
         for i in range(6):
             if i == 0:
                 mask = cv2.inRange(hsv_img, self.lower[i], self.upper[i])
@@ -260,10 +275,11 @@ class colour_search(object):
         if (self.lasttimeran == 0):
                 self.lasttimeran = self.lasttime.secs
                 
-        # print(hsv_img[0][0])
+        
+        middle = math.floor(c_width / 2)
         """add code to make it only do this after x seconds just in case"""
         for i in range(6):
-            if(self.lower[i][0]<=hsv_img[25][25][0] and self.upper[i][0]>=hsv_img[25][25][0] and self.lower[i][1]<=hsv_img[25][25][1] and self.upper[i][1]>=hsv_img[25][25][1] and self.target_colour == "" and (self.lasttime.secs - self.lasttimeran >= 6)):
+            if(self.lower[i][0]<=hsv_img[25][middle][0] and self.upper[i][0]>=hsv_img[25][middle][0] and self.lower[i][1]<=hsv_img[25][middle][1] and self.upper[i][1]>=hsv_img[25][middle][1] and self.target_colour == "" and (self.lasttime.secs - self.lasttimeran >= 6)):
                 self.target_colour = (self.colours[i])
                 self.state = State.RM
                 print("Target colour is:" + self.target_colour)
@@ -271,8 +287,9 @@ class colour_search(object):
                 self.target_colour = "Red"
                 print("Target colour is:" + self.target_colour)
 
+        """
         for i in range(6):
-            if(self.lower[i][0]<=hsv_img[25][25][0] and self.upper[i][0]>=hsv_img[25][25][0] and self.lower[i][1]<=hsv_img[25][25][1] and self.upper[i][1]>=hsv_img[25][25][1] and self.target_colour != "" and (self.lasttime.secs - self.lasttimeran >= 10)):
+            if(self.lower[i][0]<=hsv_img[25][middle][0] and self.upper[i][0]>=hsv_img[25][middle][0] and self.lower[i][1]<=hsv_img[25][middle][1] and self.upper[i][1]>=hsv_img[25][middle][1] and self.target_colour != "" and (self.lasttime.secs - self.lasttimeran >= 10)):
                 print(self.colours[i])
                 print(self.cy)
                 #if (self.target_colour == (self.colours[i]) and self.cy >= 80 and self.cy <= 120):
@@ -282,6 +299,14 @@ class colour_search(object):
                     self.vel_cmd.angular.z = 0
                     self.pub.publish(self.vel_cmd)
                     self.state = State.BEACON
+        """
+
+        if (self.target_colour != "" and (self.lasttime.secs - self.lasttimeran >= 10) and self.cy > 0 and self.cy < 1100):
+            print("Found beacon")
+            self.vel_cmd.linear.x = 0
+            self.vel_cmd.angular.z = 0
+            self.pub.publish(self.vel_cmd)
+            self.state = State.BEACON
                     
                 
 
@@ -307,19 +332,22 @@ class colour_search(object):
         
         while not self.ctrl_c and (self.state != State.FINDCOLOUR) and self.move_rate != "stop":
             if (self.state == State.BEACON):
-                self.pub.publish(self.vel_cmd)
                 
-                if (self.cy >= 80 and self.cy <= 100):
+                if (self.cy >= c_width * (9/20) and self.cy <= c_width * (11/20)):
                     self.vel_cmd.angular.z = 0
-                elif (self.cy<80 or self.distance_to_RightWall(self.ranges) <= 0.4):
+                    self.move_rate = "fast"
+                elif (self.cy == 0):
+                    self.vel_cmd.angular.z = 0
+                elif (self.cy< c_width * (9/20)):
                     print("Turning left")
-                    self.vel_cmd.angular.z = 0.8
-                elif (self.distance_to_LeftWall(self.ranges) <= 0.4 or self.cy>100):
+                    self.vel_cmd.angular.z = 0.3
+                elif (self.cy>c_width * (11/20)):
                     print("Turning right")
-                    self.vel_cmd.angular.z = -0.8
+                    self.vel_cmd.angular.z = -0.3
 
-                if (self.distance_to_Beacon(self.ranges)<0.45):
-                # blob detected
+                if (self.distance_to_Beacon(self.ranges)<0.375):
+                #if (np.amin(np.append(self.ranges[0:30],self.ranges[330:360])) <= 0.375):
+                    # blob detected
                     self.move_rate = "stop"
                         
                     
@@ -327,10 +355,10 @@ class colour_search(object):
                 else:
                     self.move_rate = "slow"
                 if self.move_rate == "fast":
-                    self.vel_cmd.linear.x = 0.3
+                    self.vel_cmd.linear.x = 0.4
                 elif self.move_rate == "slow":
                     print("Slow")
-                    self.vel_cmd.linear.x = 0
+                    self.vel_cmd.linear.x = 0.25
                 elif self.move_rate == "stop":    
                     print("Stop")                
                     self.vel_cmd.linear.x = 0.2
@@ -341,32 +369,32 @@ class colour_search(object):
             
             else:
                 state = self.state_cal(self.ranges)
-            if (np.amin(np.append(self.ranges[0:30], self.ranges[330:360])) <= 0.4 and (self.ranges[55] >= 0.4 or self.ranges[-55] >= 0.4)):
-                self.move_speed_setting(0, 1)
-            elif (state == State.SW):
-                front_right = self.ranges[55]
-                e  = 0.4 - front_right
-                kp = 3
-                self.move_speed_setting(0.25, kp *e)
-            elif (state == State.RT):
-                self.move_speed_setting(0.25, -0.9)
+                if (np.amin(np.append(self.ranges[0:30], self.ranges[330:360])) <= 0.4 and (self.ranges[55] >= 0.4 or self.ranges[-55] >= 0.4)):
+                    self.move_speed_setting(0, 1)
+                elif (state == State.SW):
+                    front_right = self.ranges[55]
+                    e  = 0.4 - front_right
+                    kp = 3
+                    self.move_speed_setting(0.25, kp *e)
+                elif (state == State.RT):
+                    self.move_speed_setting(0.25, -0.9)
 
-            elif (state == State.LT):
-                self.move_speed_setting(0.25, 1.5)
+                elif (state == State.LT):
+                    self.move_speed_setting(0.25, 1.5)
 
-            elif (state == State.END):
-                self.move_speed_setting(0,1.5)
-            elif (state == State.RT or state == State.RT):
-                self.move_speed_setting(0.25, -0.9)
+                elif (state == State.END):
+                    self.move_speed_setting(0,1.5)
+                elif (state == State.RT or state == State.RT):
+                    self.move_speed_setting(0.25, -0.9)
 
-            elif (state == State.LT or state == State.LT):
-                self.move_speed_setting(0.25, 1.5)
+                elif (state == State.LT or state == State.LT):
+                    self.move_speed_setting(0.25, 1.5)
 
-            elif (state == State.END):
-                self.move_speed_setting(0, 1.5)
+                elif (state == State.END):
+                    self.move_speed_setting(0, 1.5)
 
-            else:
-                self.move_speed_setting(0, 0)
+                else:
+                    self.move_speed_setting(0, 0)
 
             self.publish()
             self.rate.sleep()
